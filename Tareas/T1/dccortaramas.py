537bd636150f61c292315bd0e004e96b61a5c533
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-03-22 14:41:57 -0300

    add balance with min cost and refactor other functions accordingly

--- Tareas/T1/dccortaramas.py
+++ Tareas/T1/dccortaramas.py
@@ -1,5 +1,7 @@
 import utilidades
 from pathlib import Path
+from collections import deque
+
 
 # Files constants
 READ_FOLDER = "data"
@@ -29,13 +31,15 @@ class Bonsai:
 
         with path.open(mode="r", encoding="utf-8") as file:
             for line in file:
-                node, hasFlowerText, canEditText, childsText = line.strip().split(",")
+                id, hasFlowerText, canEditText, childsText = line.strip().split(",")
 
                 hasFlower = hasFlowerText == "T"
                 canEdit = canEditText == "T"
                 childs = childsText.split(";")
 
-                self.estructura.append([node, hasFlower, canEdit, childs])
+                node = [id, hasFlower, canEdit, childs]
+
+                self.estructura.append(node)
 
     def visualizar_bonsai(
         self, orientacion: str, emojis: bool, guardar_archivo: bool
@@ -60,23 +64,33 @@ class Bonsai:
 
         return None
 
-    def find_node(self, id: int) -> list | None:
+    def find_node(self, id: str) -> list | None:
+        for node in self.estructura:
+            if node[0] == id:
+                return node
+
+        return None
+
+    def find_parent(self, id: str) -> list | None:
+        for node in self.estructura:
+            if id in node[3]:
+                return node
+
+        return None
+
+    def remove_node(self, id: str) -> None:
         index = self.find_node_index(id)
 
-        if index is None:
-            return None
-
-        return self.estructura[index]
-
-    def remove_node(self, id: int) -> None:
-        index = self.find_node_index(id)
         self.estructura.pop(index)
 
-        for node in self.estructura:
-            for i in range(2):
-                if node[3][i] == id:
-                    node[3][i] = "0"
-                    break
+        parent = self.find_parent(id)
+
+        if parent is None:
+            return
+
+        for i in range(2):
+            if parent[3][i] == id:
+                parent[3][i] = "0"
 
     def copy(self):
         tree = Bonsai(self.identificador, self.costo_corte, self.costo_flor, [])
@@ -128,70 +142,120 @@ class DCCortaRamas:
         return DONE
 
     def es_simetrico(self, bonsai: Bonsai) -> bool:
-        def search(node: list | None, swap: bool):
-            if node is None:
-                return ()
+        solution = self.balance(bonsai)
 
-            left, right = node[3]
+        cost = solution[0]
 
-            if swap:
-                left, right = right, left
+        return cost == 0
 
-            left_child = bonsai.find_node(left)
-            right_right = bonsai.find_node(right)
+    def upper_bound_to_balance(self, bonsai: Bonsai) -> int:
+        return max(bonsai.costo_corte, bonsai.costo_flor) * len(bonsai.estructura)
 
-            return (node[1], search(left_child, swap), search(right_right, swap))
+    def balance(self, bonsai: Bonsai) -> list:
+        upper_bound = self.upper_bound_to_balance(bonsai)
 
-        root = bonsai.estructura[0]
+        def merge_solutions(a: list, b: list):
+            return [a[0] + b[0], [*a[1], *b[1]]]
 
-        left_child = bonsai.find_node(root[3][0])
-        right_child = bonsai.find_node(root[3][1])
+        def search(node_a: list | None, node_b: list | None):
+            if node_a is None and node_b is None:
+                return [0, []]
 
-        return search(left_child, True) == search(right_child, False)
+            if node_a is None and node_b is not None:
+                copy = bonsai.copy()
+                can_remove = self.quitar_nodo(copy, node_b[0]) == DONE
+
+                if not can_remove:
+                    return [upper_bound, []]
+
+                return [bonsai.costo_corte, [[REMOVE_NODE, node_b[0]]]]
+
+            if node_b is None and node_a is not None:
+                copy = bonsai.copy()
+                can_remove = self.quitar_nodo(copy, node_a[0]) == DONE
+
+                if not can_remove:
+                    return [upper_bound, []]
+
+                return [bonsai.costo_corte, [[REMOVE_NODE, node_a[0]]]]
+
+            left_a, right_a = node_a[3]
+            left_b, right_b = node_b[3]
+
+            left_child_a = bonsai.find_node(left_a)
+            right_child_a = bonsai.find_node(right_a)
+
+            left_child_b = bonsai.find_node(left_b)
+            right_child_b = bonsai.find_node(right_b)
+
+            best_solution = [upper_bound, []]
+
+            subtree_solution = merge_solutions(
+                search(left_child_a, right_child_b), search(right_child_a, left_child_b)
+            )
+
+            if node_a[1] == node_b[1]:
+                best_solution = min(best_solution, subtree_solution)
+
+            if node_a[1] != node_b[1]:
+                if node_a[2]:
+                    solution = merge_solutions(
+                        [bonsai.costo_flor, [[MODIFY_FLOWER, node_a[0]]]],
+                        subtree_solution,
+                    )
+
+                    best_solution = min(best_solution, solution)
+
+                elif node_b[2]:
+                    solution = merge_solutions(
+                        [bonsai.costo_flor, [[MODIFY_FLOWER, node_b[0]]]],
+                        subtree_solution,
+                    )
+
+                    best_solution = min(best_solution, solution)
+
+            copy = bonsai.copy()
+            can_remove = (
+                self.quitar_nodo(copy, node_a[0]) == DONE
+                and self.quitar_nodo(copy, node_b[0]) == DONE
+            )
+
+            if can_remove:
+                solution = [
+                    2 * bonsai.costo_corte,
+                    [[REMOVE_NODE, node_a[0]], [REMOVE_NODE, node_b[0]]],
+                ]
+
+                best_solution = min(best_solution, solution)
+
+            return best_solution
+
+        root = bonsai.find_node("1")
+
+        left = bonsai.find_node(root[3][0])
+        right = bonsai.find_node(root[3][1])
+
+        return search(left, right)
 
     def emparejar_bonsai(self, bonsai: Bonsai) -> list:
-        stack = [(bonsai.copy(), set(), [])]
+        cost, instrucctions = self.balance(bonsai)
 
-        while stack:
-            tree, used, logs = stack.pop(0)
+        if cost >= self.upper_bound_to_balance(bonsai):
+            return [False, []]
 
-            if self.es_simetrico(tree):
-                return [True, logs]
-
-            for node in tree.estructura:
-                if not node[2] or node[0] in used:
-                    continue
-                to_remove = tree.copy()
-                to_modify = tree.copy()
-
-                can_remove = self.quitar_nodo(to_remove, node[0]) == DONE
-                can_modify = self.modificar_nodo(to_modify, node[0]) == DONE
-
-                if can_remove:
-                    stack.append(
-                        (
-                            to_remove,
-                            used | {node[0]},
-                            [*logs, [REMOVE_NODE, node[0]]],
-                        )
-                    )
-
-                if can_modify:
-                    stack.append(
-                        (
-                            to_modify,
-                            used | {node[0]},
-                            [*logs, [MODIFY_FLOWER, node[0]]],
-                        )
-                    )
-
-        return [False, []]
+        return [True, instrucctions]
 
     def emparejar_bonsai_ahorro(self, bonsai: Bonsai) -> list:
-        pass
+        cost, instrucctions = self.balance(bonsai)
+
+        if cost >= self.upper_bound_to_balance(bonsai):
+            return [False, []]
+
+        return [True, cost, instrucctions]
 
     def comprobar_solucion(self, bonsai: Bonsai, instrucciones: list) -> list:
         copy = bonsai.copy()
+
         cost = 0
 
         for type, id in instrucciones:
