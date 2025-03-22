e359f92fd027092e0844fa583c9fb5bb79d4d3ff
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-03-22 15:50:27 -0300

    add menu balance

--- Tareas/T1/main.py
+++ Tareas/T1/main.py
@@ -14,11 +14,53 @@ def print_menu(options: list[str]) -> int:
 
     numbers = str(list(range(1, len(options) + 1)))[1:-1]
 
-    return input(f"Indique su opción ({numbers}):")
+    return input(f"Indique su opción ({numbers}): ")
 
 
 def erase_prints():
     print("\n" * 100)
+
+
+def print_tree_change(before: Bonsai, after: Bonsai):
+    print()
+    print("Bonsái original:")
+    print()
+
+    before.visualizar_bonsai("Vertical", True, False)
+
+    print()
+    print("Bonsái modificado:")
+    print()
+
+    after.visualizar_bonsai("Vertical", True, False)
+
+    print()
+
+
+def ask_yes_no(question: str) -> bool:
+    print()
+
+    while True:
+        answer = input(f"{question} (Si/No): ").strip().lower()
+
+        if answer == "s" or answer == "si":
+            return True
+
+        if answer == "n" or answer == "no":
+            return False
+
+        print("Respuesta no válida.")
+
+
+def ask_number(question: str) -> int:
+
+    while True:
+        answer = input(f"{question}: ").strip()
+
+        if answer.isdigit():
+            return int(answer)
+
+        print("Respuesta no válida.")
 
 
 def main():
@@ -42,7 +84,7 @@ def main():
         print("El archivo no existe en la carpeta indicada.")
         return
 
-    tree = Bonsai("1", 0, 0, [])
+    tree = Bonsai("1", 1, 1, [])
     corta_ramas = DCCortaRamas()
     tree.cargar_bonsai_de_archivo(folder, filename)
 
@@ -98,10 +140,56 @@ def main():
                 print("El bonsái no es simétrico.")
 
         if answer == 5:
-            print("Aún no esta implementado.")
+            if corta_ramas.es_simetrico(tree):
+                print("El bonsái ya está podado.")
+            else:
+
+                with_min_cost = ask_yes_no("¿Podar con el costo mínimo?")
+
+                tree_copy = tree.copy()
+
+                if with_min_cost:
+                    cut_cost = ask_number("Ingrese el costo de corte")
+                    flower_cost = ask_number("Ingrese el costo de modificar una flor")
+
+                    tree.costo_corte = cut_cost
+                    tree.costo_flor = flower_cost
+
+                    solution = corta_ramas.emparejar_bonsai_ahorro(tree)
+
+                    tree.costo_corte = 1
+                    tree.costo_flor = 1
+
+                    was_balanced = solution[0]
+
+                    if was_balanced:
+                        cost, changes = solution[1:]
+
+                        corta_ramas.apply_solution(tree, changes)
+
+                        print(
+                            f"El bonsái fue podado con costo mínimo de {cost} y {len(changes)} cambios"
+                        )
+
+                        print_tree_change(tree_copy, tree)
+                    else:
+                        print("No es posible podar el bonsái.")
+                else:
+                    was_balanced, changes = corta_ramas.emparejar_bonsai(tree)
+
+                    if was_balanced:
+                        print(f"El bonsái fue podado con {len(changes)} cambios")
+
+                        corta_ramas.apply_solution(tree, changes)
+
+                        print_tree_change(tree_copy, tree)
+                    else:
+                        print("No es posible podar el bonsái.")
+
+        print()
 
         if 1 <= answer <= 5:
-            input("Presione enter para volver al menu.")
+            input("Presione enter para volver al menu. ")
         else:
             print("Opción no válida.")
 
